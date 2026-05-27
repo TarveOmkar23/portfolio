e91e0780d47b92610fbb8763d285a6f25871cfed
TarveOmkar23 <omkartarve23@gmail.com>
2026-05-27 14:35:06 +0530

    Resume Download

--- src/App.jsx
+++ src/App.jsx
@@ -219,7 +219,7 @@ function App() {
             </a>
 
            <a
-              href="/resume.pdf"
+              href="/Omkar_Tarve.pdf"
               download
               className="border border-gray-400 px-6 py-3 rounded-xl hover:bg-gray-100 hover:scale-105 transition duration-300"
             >
